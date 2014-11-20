0eb8d98822855fc98f700baef544d1d230a75523
Elemnir <ahoward0920@gmail.com>
2014-11-19 22:22:34 -0500

    Started work on Lab3

---
+++ lab3/gen.py
@@ -0,0 +1,5 @@
+from random import uniform
+from sys import argv
+
+for i in range(int(argv[1])):
+    print uniform(0.0, 100.0)
---
+++ lab3/task1_serial.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+
+int main(int argc, char** argv)
+{
+    if ((argc > 1 && (argv[1] == "-h" || argv[1] == "--help")) || argc != 3)
+    {
+        std::cerr << "Usage: t1s [-h|--help] N filename\n";
+        return -1;
+    }
+
+    std::ifstream fin(argv[2]);
+    if (fin.fail())
+    {
+        perror(argv[2]);
+        return -1;
+    }
+    int n;
+    std::stringstream(argv[1]) >> n;
+
+    float val;
+    std::vector<float> data, output;
+    while (fin >> val)
+        data.push_back(val);
+    
+    output.resize(data.size() - n, 0.0f);
+    for (unsigned i = 0; i < data.size() - n; ++i)
+        for (int j = 0; j < n; ++j)
+            output[i] += data[i + j];
+
+    int m = 0;
+    for (unsigned i = 1; i < output.size(); ++i)
+        if (output[i] > output[m])
+            m = i;
+
+    std::cout << "Index: " << std::setw(10) << m << std::endl;
+    std::cout << "Sum:   " << std::setw(10) << output[m] << std::endl;
+}
